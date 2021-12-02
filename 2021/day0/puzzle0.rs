23bc0fd0a3e31a65c3175fae2ceaa2671538adc2
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-12-02 17:30:19 +0100

    can't do rust cuz of IDEA, thats sad

--- 2021/day0/puzzle0.rs
+++ 2021/day0/puzzle0.rs
@@ -1,3 +1,26 @@
-fn main() {
+use std::fs::File;
+use std::io::prelude::*;
 
+fn main() -> std::io::Result<()> {
+	let mut file = File::open("test")?;
+	let mut contents = String::new();
+	file.read_to_string(&mut contents)?;
+
+	let mut depths: Vec<i32> = Vec::new();
+	let mut raw_depths: Vec<String> = contents.split(' ').map(|s| s.to_string()).collect();
+
+	for line in raw_depths {
+		depths.append( line.parse().unwrap() )
+	}
+
+	let mut count = 0;
+	let mut last_depth = depths.remove(0);
+
+	for depth in depths {
+		count += ( depth > last_depth ) as i32;
+	}
+
+	println!("{}", count);
+
+	Ok(())
 }
