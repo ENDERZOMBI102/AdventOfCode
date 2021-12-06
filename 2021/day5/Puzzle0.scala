76396b52622e5e68709c5230f79325855c1cfbb0
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-12-06 11:26:01 +0100

    day6 initial

--- 2021/day5/Puzzle0.scala
+++ 2021/day5/Puzzle0.scala
@@ -1,6 +1,31 @@
 import scala.io.Source
 
-def main(): Unit = {
-	val lines = Source.fromFile( "./2021/day4/test" ).getLines()
+object Puzzle0 {
+	def main(args: Array[String] ): Unit = {
+		var nums = Source.fromFile("./2021/day5/input")
+			.getLines()
+			.next()
+			.split(",")
+			.map(Integer.valueOf)
+			.toList;
+		var day = 0;
 
+		println(s"Initial state $nums")
+		while (day < 80) {
+			day += 1;
+			val max = nums.length - 1;
+
+			for ( i <- 0 to max ) {
+				if (nums(i) == 0) {
+					nums = nums.updated(i, 6);
+					nums = nums.appended(8);
+				} else {
+					nums = nums.updated(i, nums(i) - 1);
+				}
+			}
+
+			println(s"After $day ${if (day == 1) "day" else "days"}: $nums")
+		}
+		println(s"There is ${nums.length} fishes")
+	}
 }
--- 2021/day5/Puzzle1.scala
+++ 2021/day5/Puzzle1.scala
@@ -1,6 +1,7 @@
 import scala.io.Source
+object Puzzle1 {
+	def main(): Unit = {
+		val lines = Source.fromFile("./2021/day5/test").getLines()
 
-def main(): Unit = {
-	val lines = Source.fromFile( "./2021/day4/test" ).getLines()
-
+	}
 }
