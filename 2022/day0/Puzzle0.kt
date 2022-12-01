a83051bd817b2f9773a8378098e03af6c4255b5a
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-12-02 00:39:01 +0100

    2022 day 0!

---
+++ 2022/day0/Puzzle0.kt
@@ -0,0 +1,19 @@
+package day0
+
+import java.nio.file.Files
+import java.nio.file.Path
+
+fun main( argv: Array<String> ) {
+    val elfs = mutableListOf(0)
+    val lines = Files.readAllLines( Path.of("./2022/day0/input") )
+    var index = 0
+
+    lines.forEach { line ->
+        if ( line == "" ) {
+            elfs.add(0)
+            index += 1
+        } else
+            elfs[index] += Integer.valueOf( line )
+    }
+    println( elfs.max() )
+}
---
+++ 2022/day0/Puzzle1.kt
@@ -0,0 +1,22 @@
+package day0
+
+import java.nio.file.Files
+import java.nio.file.Path
+
+fun main( argv: Array<String> ) {
+    val elfs = mutableListOf( 0 )
+    val lines = Files.readAllLines( Path.of("./2022/day0/input") )
+    var index = 0
+
+    lines.forEach { line ->
+        if ( line == "" ) {
+            elfs.add(0)
+            index += 1
+        } else
+            elfs[index] += Integer.valueOf( line )
+    }
+
+    println( listOf( elfs.removeMax(), elfs.removeMax(), elfs.removeMax() ).sum() )
+}
+
+fun MutableList<Int>.removeMax(): Int = this.max().also { this@removeMax.remove(it) }
