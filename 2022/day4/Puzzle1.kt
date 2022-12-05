cb61c06d4a1ea0cac70feb28e6f3e5a7ebaf1154
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-12-05 14:15:42 +0100

    day 4 ( unfinished, uploaded to ask for help )

---
+++ 2022/day4/Puzzle1.kt
@@ -0,0 +1,39 @@
+package day4
+
+import java.nio.file.Files
+import java.nio.file.Path
+
+fun main() {
+	// TODO: Do this part
+	val score = Files.readAllLines( Path.of( "./2022/day4/input" ) )
+		.run {
+			val numLine = indexOf("") - 1
+			val integer = { value: String -> value.takeIf { it.all( Char::isDigit ) }?.toInt() }
+
+			// make grid
+			val grid = mutableListOf<MutableList<Char>>()
+			var x = 1
+
+			while ( x < this[numLine].length ) {
+				if ( this[numLine][x].isDigit() ) {
+					var y = numLine
+					val column = mutableListOf<Char>()
+					while ( --y >= 0 && this[y].length > x )
+						this[y][x].takeIf( Char::isLetter )?.also { column.add( it ) }
+					grid.add( column )
+				}
+				x++
+			}
+
+			printGrid( grid )
+
+			// pass into lines exec mode
+			subList( numLine + 2, size )
+				.map { line -> line.split(" ").mapNotNull { integer( it ) } }
+				.forEach { ( amount, from, to ) ->
+					grid[ to - 1 ] += grid[ from - 1 ].run { takeLast( amount ).also { removeAll( it ) }.reversed() }
+				}
+			grid.map { it.lastOrNull() ?: " " }.joinToString( separator = "" )
+		}
+	println( score ) //
+}
